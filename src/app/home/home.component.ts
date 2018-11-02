68b283b180e43af045cb8b8096eac618fc546356
Kevin Lewis <kevin.lewis_22@hotmail.com>
2018-11-01 20:56:04 -0400

    validation on sign up work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,12 +25,14 @@ export class HomeComponent implements OnInit {
   firstname:string;
   lastname:string;
 
+  attempt:signupObject;
+
+  displayMessage:boolean;
+  validationMessage:string[];
 
   constructor(private router: Router, private fb: FormBuilder, private http: HttpClient) {
-    this.signupSubmitted = false;
 
     router.events.subscribe((val) => {
-        // see also
         // document.body.style.background = '#393E41';
         document.body.style.background = 'url(\'../../assets/mountains.jpg\') no-repeat center center fixed';
         document.body.style.backgroundSize = 'cover';
@@ -40,27 +42,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    // if(window.innerWidth > 576) {
-    //   document.getElementById('triangle').style.borderLeft = String(window.innerWidth/25) + 'em solid green';
-    //   document.getElementById('triangle').style.borderTop = '20em solid transparent';
-    //   document.getElementById('triangle2').style.borderBottom = '20em solid transparent';
-    // } else {
-    //   document.getElementById('triangle').style.borderLeft = String(window.innerWidth/32) + 'em solid transparent';
-    //   document.getElementById('triangle').style.borderRight = String(window.innerWidth/32) + 'em solid transparent';
-    //   document.getElementById('triangle').style.borderTop = String(window.innerWidth/75) + 'em solid green';
-    //
-    //   document.getElementById('triangle2').style.borderLeft = String(window.innerWidth/32) + 'em solid transparent';
-    //   document.getElementById('triangle2').style.borderRight = String(window.innerWidth/32) + 'em solid transparent';
-    //   document.getElementById('triangle2').style.borderBottom = String(window.innerWidth/75) + 'em solid green';
-    // }
-
-    // this.signupForm = this.fb.group({
-    //   email: [this.email, Validators.required],
-    //   password: [this.password, Validators.required, Validators.minLength(7)],
-    //   rePassword: [this.repassword, Validators.required, Validators.minLength(7)],
-    //   firstName: [this.firstname, Validators.required],
-    //   lastName: [this.lastname, Validators.required]
-    // });
     this.signupForm = this.fb.group({
         'email': [this.email, Validators.required],
         'password': [this.password, Validators.required],
@@ -71,12 +52,20 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.signupForm.value);
+    // console.log(this.signupForm.value);
 
     // validate form
-
-    // attempt to sign up
-
+    var validated = this.validateSignUpForm(this.signupForm);
+    console.log(validated);
+    // if valid, attempt to signup
+    if (validated.status) {
+      // attempt to sign up
+    } else {
+      // display message
+      this.displayMessage = true;
+      this.validationMessage = validated.message;
+      console.log(validated.message);
+    }
   }
 
   @HostListener('window:resize', ['$event'])
@@ -89,4 +78,58 @@ export class HomeComponent implements OnInit {
     //
     // }
   }
+
+  validateSignUpForm(s:FormGroup) {
+    console.log(s);
+    let messageList: Array<string> = [];
+    let status: boolean = true;
+    if (s.get('email').invalid) {
+      status = false;
+      messageList.push('Email invalid.');
+    }
+    if (s.get('password').invalid) {
+      console.log('checking password....');
+      status = false;
+      messageList.push('Password invalid.');
+    }
+    if (s.get('password').value !== s.get('rePassword').value) {
+      status = false;
+      messageList.push('Passwords do not match.');
+    }
+    if (s.get('firstName').invalid) {
+      status = false;
+      messageList.push('First name invalid.');
+    }
+    if (s.get('lastName').invalid) {
+      status = false;
+      messageList.push('Last name invalid.');
+    }
+    console.log(messageList);
+
+    return { status: status, message: messageList };
+
+    // this.displayMessage = false;
+    // this.validationMessage = "";
+    // console.log(s);
+    // // check for nulls
+    // if((s.email === null || s.email === "") || (s.password === null || s.password === "") || (s.repassword === null || s.repassword === "") || (s.firstname === null || s.firstname === "") || (s.lastname === null || s.lastname === "")) {
+    //   return {
+    //     status: false,
+    //     message: "Required fields missing."
+    //   };
+    // } else if (s.password != s.repassword) {
+    //   return {
+    //     status: false,
+    //     message: "Passwords do not match."
+    //   };
+    // }
+  }
+}
+
+export interface signupObject {
+  email: string,
+  password: string,
+  repassword: string,
+  firstname: string,
+  lastname: string
 }
