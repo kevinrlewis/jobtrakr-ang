2157f03921ce8e49564fe28f82ed1dd0441429a3
kevin <kevin.lewis_22@hotmail.com>
2018-11-01 22:59:18 -0400

    finished sign up validation - started login page form - started login form validation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,8 +10,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  public innerWidth: any;
-  public innerHeight: any;
+  // public innerWidth: any;
+  // public innerHeight: any;
   public triangle: any;
 
   public title: string;
@@ -33,22 +33,22 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router, private fb: FormBuilder, private http: HttpClient) {
 
     router.events.subscribe((val) => {
-        // document.body.style.background = '#393E41';
-        document.body.style.background = 'url(\'../../assets/mountains.jpg\') no-repeat center center fixed';
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.height = '100%';
-        console.log(val instanceof NavigationEnd);
+      // document.body.style.background = '#393E41';
+      document.body.style.background = 'url(\'../../assets/mountains.jpg\') no-repeat center center fixed';
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.height = '100%';
+      console.log(val instanceof NavigationEnd);
     });
   }
 
   ngOnInit() {
     this.signupForm = this.fb.group({
-        'email': [this.email, Validators.required],
-        'password': [this.password, Validators.required],
-        'rePassword': [this.repassword, Validators.required],
-        'firstName': [this.firstname, Validators.required],
-        'lastName': [this.lastname, Validators.required]
-    })
+      'email': [this.email, [Validators.required, Validators.email]],
+      'password': [this.password, [Validators.required, Validators.minLength(7)]],
+      'rePassword': [this.repassword, [Validators.required]],
+      'firstName': [this.firstname, [Validators.required]],
+      'lastName': [this.lastname, [Validators.required]]
+    });
   }
 
   onSubmit() {
@@ -60,69 +60,58 @@ export class HomeComponent implements OnInit {
     // if valid, attempt to signup
     if (validated.status) {
       // attempt to sign up
+      this.signUp(this.signupForm.value);
     } else {
       // display message
       this.displayMessage = true;
       this.validationMessage = validated.message;
-      console.log(validated.message);
     }
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.innerWidth = window.innerWidth;
-    this.innerHeight = window.innerHeight;
-    // if(this.innerWidth > 576) {
-    //   document.getElementById('triangle-img').style.clipPath = 'polygon(0% 0%, 0% 100%, ' + (this.innerWidth/18) + '% 50%)'
-    // } else {
-    //
-    // }
-  }
+  // listen to the width and height of the screen being resized
+  // @HostListener('window:resize', ['$event'])
+  // onResize(event) {
+  //   this.innerWidth = window.innerWidth;
+  //   this.innerHeight = window.innerHeight;
+  // }
 
-  validateSignUpForm(s:FormGroup) {
-    console.log(s);
+  private validateSignUpForm(s:FormGroup) {
+    // variables to return
     let messageList: Array<string> = [];
     let status: boolean = true;
+
+    // check if the email is invalid
     if (s.get('email').invalid) {
       status = false;
       messageList.push('Email invalid.');
     }
+    // check if password is invalid
+    // or if passwords do not match
     if (s.get('password').invalid) {
-      console.log('checking password....');
       status = false;
       messageList.push('Password invalid.');
-    }
-    if (s.get('password').value !== s.get('rePassword').value) {
+    } else if (s.get('password').value !== s.get('rePassword').value) {
       status = false;
       messageList.push('Passwords do not match.');
     }
+    // check if the first name field is valid
     if (s.get('firstName').invalid) {
       status = false;
       messageList.push('First name invalid.');
     }
+    // check if the last name field is valid
     if (s.get('lastName').invalid) {
       status = false;
       messageList.push('Last name invalid.');
     }
-    console.log(messageList);
 
+    // return object
     return { status: status, message: messageList };
+  }
 
-    // this.displayMessage = false;
-    // this.validationMessage = "";
-    // console.log(s);
-    // // check for nulls
-    // if((s.email === null || s.email === "") || (s.password === null || s.password === "") || (s.repassword === null || s.repassword === "") || (s.firstname === null || s.firstname === "") || (s.lastname === null || s.lastname === "")) {
-    //   return {
-    //     status: false,
-    //     message: "Required fields missing."
-    //   };
-    // } else if (s.password != s.repassword) {
-    //   return {
-    //     status: false,
-    //     message: "Passwords do not match."
-    //   };
-    // }
+  private signUp(formValues:object) {
+    // console.log(formValues);
+    // call api
   }
 }
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,11 @@ import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) {
+  loginForm:FormGroup;
+  email:string;
+  password:string;
+
+  constructor(private router: Router, private fb: FormBuilder, private http: HttpClient) {
     router.events.subscribe((val) => {
         // document.body.style.background = 'rgb(54, 73, 78, 1)';
         document.body.style.background = 'url(\'../../assets/mountains.jpg\') no-repeat center center fixed';
@@ -18,6 +24,15 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      'email': [this.email, [Validators.required, Validators.email]],
+      'password': [this.password, [Validators.required]],
+    });
   }
 
+  onSubmit() {
+    // validate form
+    console.log(this.loginForm.value);
+    // attempt to login
+  }
 }
