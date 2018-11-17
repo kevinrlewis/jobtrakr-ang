726cffca9055491f170a94d0bcd959be8633f809
kevin <kevin.lewis_22@hotmail.com>
2018-11-16 22:24:35 -0500

    started to display data on the profile page, called the new get user endpoint

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import * as jwt_decode from "jwt-decode";
 import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    // 'Access-Control-Allow-Credentials': 'true'
+  }),
+  // withCredentials: true,
+  // credentials: 'include'
+};
 
 @Component({
   selector: 'app-profile',
@@ -11,7 +21,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private cookieService: CookieService, private router: Router) {
+  // @Input() email: string;
+  id: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+
+  constructor(private http: HttpClient, private cookieService: CookieService, private router: Router) {
     router.events.subscribe((val) => {
         // document.body.style.background = 'rgb(54, 73, 78, 1)';
         document.body.style.background = 'url(\'../../assets/mountains.jpg\') no-repeat center center fixed';
@@ -22,7 +38,30 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit() {
     const token = this.cookieService.get('SESSIONID');
-    console.log("cookies: ", this.cookieService.getAll());
+    // console.log("cookies: ", this.cookieService.getAll());
+
+    this.id = jwt_decode(token).sub.toString();
+
+    this.http.get<GetUserResponse>(
+      '/api/user/id/' + this.id,
+      httpOptions
+    ).subscribe(data => {
+      console.log(data);
+
+      this.email = data.data.email;
+      this.firstname = data.data.firstname;
+      this.lastname = data.data.lastname
+    });
   }
 
+}
+
+interface GetUserResponse {
+  message: string,
+  data: {
+    user_id: number,
+    email: string,
+    firstname: string,
+    lastname: string
+  }
 }
